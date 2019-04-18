3a4782535214e09cff4a9bf59b54b7bcb698393d
51MARGL <serhiy.kudryashov@gmail.com>
2019-04-18 18:45:30 +0200

    inventory start

---
+++ Assets/Scripts/ECS/Components/Items/ItemsManagerComponent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace BeyondPixels.ECS.Components.Items
+{
+    public class ItemsManagerComponent : MonoBehaviour
+    {
+        public static ItemsManagerComponent Instance { get; private set; }
+        public ItemsStoreComponent ItemsStoreComponent{ get; private set; }
+
+        public void Start()
+        {
+            ItemsManagerComponent.Instance = this;
+            this.ItemsStoreComponent = GetComponent<ItemsStoreComponent>();
+        }
+    }
+}
---
+++ Assets/Scripts/ECS/Systems/Items/PickUpSystem.cs
@@ -0,0 +1,43 @@
+﻿using BeyondPixels.ECS.Components.Items;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+
+namespace BeyondPixels.ECS.Systems.Characters.Items
+{
+    public class PickUpSystem : JobComponentSystem
+    {
+        private struct PickUpJob : IJobProcessComponentDataWithEntity<PickUpComponent>
+        {
+            public EntityCommandBuffer.Concurrent CommandBuffer;
+
+            public void Execute(Entity entity,
+                                int index,
+                                [ReadOnly]ref PickUpComponent pickUpComponent)
+            {
+                this.CommandBuffer.AddComponent(index, pickUpComponent.ItemEntity, new PickedUpComponent
+                {
+                    Owner = pickUpComponent.Owner
+                });
+                this.CommandBuffer.DestroyEntity(index, entity);
+            }
+        }
+
+        private EndSimulationEntityCommandBufferSystem _endFrameBarrier;
+
+        protected override void OnCreateManager()
+        {
+            this._endFrameBarrier = World.Active.GetOrCreateManager<EndSimulationEntityCommandBufferSystem>();
+        }
+
+        protected override JobHandle OnUpdate(JobHandle inputDeps)
+        {
+            var handle = new PickUpJob
+            {
+                CommandBuffer = this._endFrameBarrier.CreateCommandBuffer().ToConcurrent(),
+            }.Schedule(this, inputDeps);
+            this._endFrameBarrier.AddJobHandleForProducer(handle);
+            return inputDeps;
+        }
+    }
+}
