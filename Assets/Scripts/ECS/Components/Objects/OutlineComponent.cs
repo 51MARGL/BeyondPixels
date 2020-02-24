ebb19c29b47634dec4599bd778c6b4a318339e61
51MARGL <serhiy.kudryashov@gmail.com>
2020-02-24 08:49:38 +0100

    #19 - outline

---
+++ Assets/Scripts/ECS/Components/Objects/OutlineComponent.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace BeyondPixels.ECS.Components.Objects
+{
+    public class OutlineComponent : MonoBehaviour
+    {
+        [Range(1,3)]
+        public float OutlineThickness;
+        public Color OutlineColor;
+
+        private SpriteRenderer _parentRenderer;
+        private SpriteRenderer _currentRenderer;
+
+        public void Start()
+        {
+            this._currentRenderer = this.GetComponent<SpriteRenderer>();
+            this._parentRenderer = this.transform.parent.GetComponent<SpriteRenderer>();
+        }
+
+        public void Update()
+        {
+            this._currentRenderer.sprite = this._parentRenderer.sprite;
+            this._currentRenderer.material.SetColor("_OutlineColor", this.OutlineColor);
+            this._currentRenderer.material.SetFloat("_Outline", this.OutlineThickness);
+
+        }
+    }
+}
