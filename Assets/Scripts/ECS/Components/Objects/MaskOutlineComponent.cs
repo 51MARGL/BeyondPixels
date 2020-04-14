56b77a0a0e7b4c19e237bf9e4c434a84fce4756f
51MAGRL <serhiy.kudryashov@gmail.com>
2020-04-14 21:29:00 +0200

    outline

---
+++ Assets/Scripts/ECS/Components/Objects/MaskOutlineComponent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace BeyondPixels.ECS.Components.Objects
+{
+    [RequireComponent(typeof(SpriteMask), typeof(SpriteRenderer))]
+    public class MaskOutlineComponent : MonoBehaviour
+    {
+        private SpriteRenderer _currentRenderer;
+        private SpriteMask _spriteMask;
+
+        public void Start()
+        {
+            this._currentRenderer = this.GetComponent<SpriteRenderer>();
+            this._spriteMask = this.GetComponent<SpriteMask>();
+        }
+
+        public void Update()
+        {
+            this._spriteMask.sprite = this._currentRenderer.sprite;
+        }
+    }
+}
